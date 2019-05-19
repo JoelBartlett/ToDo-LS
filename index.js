503e69f01723d44cee8d184bcdc2a70583022511
Apple <apple@Apples-MacBook-Pro-3.local>
2019-05-18 22:45:15 -0600

    Added TD LS pages

---
+++ index.js
@@ -0,0 +1,32 @@
+var cardContainer = $('.card-container');
+var submit = $('.submit-btn');
+var del = $('.del')
+
+
+
+submit.on("click", createNewUser);
+
+function createNewUser() {
+  var toDo = $('.to-do').val();
+
+  appendNewToDo(toDo);
+  clearInputs();
+}
+
+function appendNewToDo(toDo) {
+    cardContainer.append(`
+    <div class="to-do-card">
+      <ul>${toDo}</ul>
+ <button type="button" class="listButton">Done?</button>
+    </div>
+updateStorage(toDo);
+  }
+
+  function updateStorage(toDo) {
+    localStorage.setItem('toDo', toDo);
+    console.log(localStorage);
+  }
+
+function clearInputs() {
+  $('.to-do').val("");
+}
