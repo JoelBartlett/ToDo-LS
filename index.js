af17037298807f24082a88c25015dd8be18461a8
Apple <apple@Apples-MacBook-Pro-3.local>
2019-05-20 15:44:21 -0600

    More updates

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-var cardContainer = $('.card-container');
-var submit = $('.submit-btn');
-// var currentList = localStorage.getItem('toDo');
+var cardContainer = $(".card-container");
+var submit = $(".submit-btn");
+var currentList = localStorage.getItem('toDo');
 
 submit.on("click", createNewUser);
 
 function createNewUser() {
-  var toDo = $('.to-do').val();
+  var toDo = $(".to-do").val();
   appendNewToDo(toDo);
   clearInputs();
 }
@@ -16,14 +16,15 @@ function appendNewToDo(toDo) {
       <ul>${toDo}</ul>
  <button type="button" class="listButton">Done?</button>
     </div>
-  ')';}
-  //updateStorage(toDo);
+  `);
+}
+updateStorage("toDo");
 
-  //function updateStorage(toDo) {
-//    localStorage.setItem('toDo', toDo);
-//    console.log(localStorage);
-//  }
+function updateStorage(toDo) {
+    localStorage.setItem("toDo", toDo);
+    console.log(localStorage);
+  }
 
 function clearInputs() {
-  $('.to-do').val("");
+  $(".to-do").val("");
 }
